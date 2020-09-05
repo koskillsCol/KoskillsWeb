8e8f1928bdfaaf79e2f92bd8eab0923538773907
wilmar josé verjel ortega <wilmar_jose_10@hotmail.com>
2020-09-05 10:25:22 -0500

    Pesonalización

---
+++ Koskills/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Koskills.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Koskills.Areas.Identity.IdentityHostingStartup))]
+namespace Koskills.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
